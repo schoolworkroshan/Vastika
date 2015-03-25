4eedad185a88804ce8ace9c909b0f33b152d1319
Roshan Lamichhane <schoolwork.roshan@gmail.com>
2015-03-25 11:30:06 -0500

    Made some changes

---
+++ Information/Information/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Information
+//
+//  Created by Roshan Lamichhane on 3/25/15.
+//  Copyright (c) 2015 Vastika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Information/Information/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Information
+//
+//  Created by Roshan Lamichhane on 3/25/15.
+//  Copyright (c) 2015 Vastika. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    //Made some changes here
+}
+
+@end
