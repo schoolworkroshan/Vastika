b0a8274a843f16d240166e54dd09b0a4b393cbe6
Roshan Lamichhane <schoolwork.roshan@gmail.com>
2015-04-06 14:00:55 -0500

    View Controller sign in

---
+++ Information/Information/StudentViewController.swift
@@ -0,0 +1,41 @@
+//
+//  StudentViewController.swift
+//  Information
+//
+//  Created by Roshan Lamichhane on 4/6/15.
+//  Copyright (c) 2015 Vastika. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class StudentViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func studentSignOutButton(sender: AnyObject) {
+        PFUser.logOut()
+        self.dismissViewControllerAnimated(true , completion: nil)
+    }
+
+}
